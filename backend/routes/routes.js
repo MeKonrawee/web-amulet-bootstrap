31efea9024bb2522cdafd5e00a57483833091bd0
MeKonrawee <konrawee207@gmail.com>
2023-06-02 16:00:11 +0700

    backend

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -3,6 +3,10 @@ const {
   GetHistoryService: getHistory,
 } = require("../controllers/historyController");
 
+const {
+  GetAmuletService: getAmulet,
+} = require("../controllers/amuletController");
+
 const swaggerUi = require("swagger-ui-express");
 const swaggerDoc = require("../swagger");
 
@@ -11,6 +15,7 @@ const router = express.Router();
 router.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
 router.get("/history", getHistory);
+router.get("/amulet", getAmulet);
 
 module.exports = {
   routes: router,
