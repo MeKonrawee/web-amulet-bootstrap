662c58667f3aa6bc0f6f4751e36a4f910aa94694
MeKonrawee <konrawee207@gmail.com>
2023-06-13 19:11:52 +0700

    backend

--- backend/models/history.js
+++ backend/models/history.js
@@ -1,9 +1,10 @@
 class HistoryResponse {
-  constructor(id, name, image, details) {
+  constructor(id, name, image, details, map) {
     this.id = id;
     this.name = name;
     this.image = image;
     this.details = details;
+    this.map = map;
   }
 }
 
